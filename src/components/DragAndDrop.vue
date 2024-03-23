73f5fd268bed2a519ee5f9724bbb96ffc583a16e
Valentin447 <valentin@myttk.ru>
2024-03-23 22:36:49 +0700

    fix2 drag and drop

--- src/components/DragAndDrop.vue
+++ src/components/DragAndDrop.vue
@@ -1,8 +1,5 @@
 <template>
-  <div id="file-drag-drop">
-    <form class="drag-and-drop" ref="fileform">
-    </form>
-  </div>
+
 </template>
 <script>
 export default {
@@ -38,7 +35,6 @@ export default {
         e.stopPropagation();
     });
   
-    // Это самое важное событие, событие, которое дает доступ к файлам
     dropZone.addEventListener("drop", function (e) {
           e.preventDefault();
           e.stopPropagation();
@@ -46,37 +42,6 @@ export default {
           this.$emit("dropFile", file);
         }.bind(this)
       );
-    
-    // this.dragAndDrop = this.determineDragAndDrop();
-    // if (this.dragAndDrop) {
-    //   [
-    //     "drag",
-    //     "dragstart",
-    //     "dragend",
-    //     "dragover",
-    //     "dragenter",
-    //     "dragleave",
-    //     "drop",
-    //   ].forEach(
-    //     function (event) {
-    //       this.$refs.fileform.addEventListener(
-    //         event,
-    //         function (e) {
-    //           e.preventDefault();
-    //           e.stopPropagation();
-    //         }.bind(this),
-    //         false
-    //       );
-    //     }.bind(this)
-    //   );
-    //   this.$refs.fileform.addEventListener(
-    //     "drop",
-    //     function (e) {
-    //       const file = e.dataTransfer.files[0];
-    //       this.$emit("dropFile", file);
-    //     }.bind(this)
-    //   );
-    // }
   },
 };
 </script>
